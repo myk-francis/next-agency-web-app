f682fbd1f725ee2232ae0343e8269db3dd52fd80
myk <michaelmugendi1707@gmail.com>
2024-01-27 20:40:18 +0300

    Update: finish home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+import React from "react";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          sequi molestiae excepturi, placeat, quo totam quia eius eos, facere
+          expedita minus quae.
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src={"/brands.png"} alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src={"/hero.gif"} alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 }
