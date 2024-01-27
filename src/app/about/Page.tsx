e218410f570fd450cb3b6a9c952ec88a71a4d6f7
myk <michaelmugendi1707@gmail.com>
2024-01-27 20:06:46 +0300

    Update: about page to show image

--- src/app/about/Page.tsx
+++ src/app/about/Page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
 import React from "react";
+import styles from "./about.module.css";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image alt="" src="/about.png" fill />
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
