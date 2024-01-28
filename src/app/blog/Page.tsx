8d46525c956c49356b103270c5ae386cc7489312
myk <michaelmugendi1707@gmail.com>
2024-01-28 11:32:39 +0300

    Update: update api to access mongodb

--- src/app/blog/Page.tsx
+++ src/app/blog/Page.tsx
@@ -1,26 +1,28 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-// import { getPosts } from "@/lib/data";
+import { getPosts } from "@/lib/data";
 
 // FETCH DATA WITH AN API
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {
-    next: { revalidate: 3600 },
-  });
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api/blog", {
+//     next: { revalidate: 3600 },
+//   });
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const BlogPage = async () => {
   // FETCH DATA WITH AN API
-  const posts = await getData();
+  // const posts = await getData();
 
   // FETCH DATA WITHOUT AN API
-  // const posts = await getPosts();
+  const posts = await getPosts();
+
+  console.log(posts);
 
   return (
     <div className={styles.container}>
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,15 +5,15 @@ import { Suspense } from "react";
 import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
-const getData = async (slug: string) => {
-  const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
+// const getData = async (slug: string) => {
+//   const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 export const generateMetadata = async ({ params }: any) => {
   const { slug } = params;
@@ -30,10 +30,10 @@ const SinglePostPage = async ({ params }: any) => {
   const { slug } = params;
 
   // FETCH DATA WITH AN API
-  const post = await getData(slug);
+  // const post = await getData(slug);
 
   // FETCH DATA WITHOUT AN API
-  // const post = await getPost(slug);
+  const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
@@ -47,13 +47,13 @@ const SinglePostPage = async ({ params }: any) => {
         <div className={styles.detail}>
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
-              <PostUser userId={post.userId} />
+              <PostUser userId={post.userid} />
             </Suspense>
           )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>
-              {post.createdAt.toString().slice(4, 16)}
+              {post.createdAt?.toString().slice(4, 16)}
             </span>
           </div>
         </div>
