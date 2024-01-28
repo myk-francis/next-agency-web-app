aad73fbddcc863b23cb659084316ede93bee7512
myk <michaelmugendi1707@gmail.com>
2024-01-28 08:26:10 +0300

    Update: about page done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>MYK</div>
+      <div className={styles.text}>
+        Myk Creative thoughts agency. All rights reserved.
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
